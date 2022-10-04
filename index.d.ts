3baeeb71cefb2976693acedcb448e9cc07da8032
Rich Trott <rtrott@gmail.com>
2022-10-04 07:31:28 -0700

    chore: update ts-standard from 11.0.0 to 12.0.0

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="node" resolution-mode="require"/>
 import type { FastifyInstance } from 'fastify';
-import { Server, IncomingMessage, ServerResponse } from 'node:http';
+import { IncomingMessage, ServerResponse } from 'node:http';
 declare const SolrProxy: {
-    start: (port?: any, userOptions?: any) => Promise<FastifyInstance<Server, IncomingMessage, ServerResponse, import("fastify").FastifyBaseLogger, import("fastify").FastifyTypeProviderDefault>>;
+    start: (port?: any, userOptions?: any) => Promise<FastifyInstance<import("fastify").RawServerDefault, IncomingMessage, ServerResponse<IncomingMessage>, import("fastify").FastifyBaseLogger, import("fastify").FastifyTypeProviderDefault>>;
 };
 export default SolrProxy;
 //# sourceMappingURL=index.d.ts.map
